7f0fadf5abacde34f9b4dd10e2ec09f47cb27555
Julia Linek <julia.linek@microsoft.wsei.edu.pl>
2022-06-08 21:11:26 +0200

    Add photo and title for MainWindow

--- AdoPet-Project.WPF/AdoPet-Project.WPF/obj/Debug/net5.0-windows/Pages/CatBreedsPage.g.i.cs
+++ AdoPet-Project.WPF/AdoPet-Project.WPF/obj/Debug/net5.0-windows/Pages/CatBreedsPage.g.i.cs
@@ -101,7 +101,7 @@ namespace AdoPet_Project.WPF.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AdoPet-Project.WPF;component/pages/catbreedspage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AdoPet-Project.WPF;V1.0.0.0;component/pages/catbreedspage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\CatBreedsPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
